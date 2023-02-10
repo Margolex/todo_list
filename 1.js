079622d39199765bded73b9e18f38dd2d64432e5
Margolex <margaritalexina@gmail.com>
2023-02-10 19:01:02 +0300

    done

---
+++ 1.js
@@ -0,0 +1,142 @@
+const form = document.querySelector("form");
+const text = document.querySelector(".input_task");
+const list = document.querySelector(".list");
+const boxDel = document.querySelector(".box_delete");
+const textCross = document.querySelector(".cross");
+const boxTask = document.querySelector("li");
+const checkbox = document.querySelector(".check");
+const btnDelCom = document.querySelector(".delete_completed");
+const btnDelAll = document.querySelector(".delete_all");
+let todos = [];
+
+if (localStorage.getItem('todos')) {
+todos = JSON.parse(localStorage.getItem('todos'))
+}
+
+const renderOneTodo = (todoObj) => {
+    
+    const cssClass = todoObj.done ? "list_text box_task_done" : "list_text";
+    const cssClassCheck = todoObj.done ? "checked" : "";
+
+    return `<li id = '${todoObj.id}' class="box_task">
+                <label>
+                <input type="checkbox" class="check" ${cssClassCheck}/>
+                <p class="${cssClass}">
+                    ${todoObj.text} 
+                </p>
+                </label>
+                <button class="cross" id='delete'><img src="./❌.png" alt="" /></button>
+            </li>`;
+}
+
+
+const renderFromTodos = () => {
+    if (todos.length == 0) {
+        list.style = "display: none";
+        boxDel.style = "display:none";
+    }
+    else {
+  
+        list.innerHTML = "";
+        const res = []
+        for (let todo of todos) {
+            res.push(renderOneTodo(todo))
+        }
+        list.innerHTML = res.join('')
+        boxDel.style = "display:block";
+        list.style = "display: grid";
+    }
+};
+
+
+renderFromTodos();
+
+form.addEventListener("submit", addTask);
+ 
+function addTask(event) {
+  //отменяет отправку формы
+  event.preventDefault();
+    if (text.value !== '') {
+      const textTask = text.value;
+
+      //описываем задачу в виде объекта
+      const newTask = {
+        id: new Date().toISOString(),
+        text: textTask,
+        done: false,
+      };
+
+    //добавляем задачу в массив с задачами
+    todos.push(newTask);
+    renderFromTodos();
+        saveToLS();
+        
+
+    //очищаем поле ввода и возвращаем фокус на него
+    text.value = "";
+    text.focus();
+    }
+}
+
+//удаление задачи
+
+list.addEventListener('click', deleteTask)
+
+function deleteTask(event) {
+    if (event.target.id !== 'delete') return;
+    
+    const parentTask = event.target.closest('li');
+
+    //определить ID удаленной задачи
+    const id = parentTask.id;
+    
+    //находим индекс удаленной задачи в масссиве и удаляем ее из массива 
+    const index = todos.findIndex((task) => task.id == id);
+    todos.splice(index, 1)
+    
+    parentTask.remove();
+    saveToLS();
+}
+
+//отмечает или отменяет выполнение задачи
+list.addEventListener('click', doneTask)
+
+function doneTask(event) {
+    if (event.target.className !== 'check') return;
+        const parentTask = event.target.closest("li");
+    
+    const id = parentTask.id;
+    const task = todos.find((task) => task.id == id);
+    task.done = !task.done;
+
+    const textTask = parentTask.querySelector("p");
+    textTask.classList.toggle("box_task_done");
+    saveToLS();
+}
+
+function saveToLS() {
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+//удаляет завершенные дела
+btnDelCom.addEventListener("click", delComplited);
+
+function delComplited() {
+    todos = todos.filter(el => !el.done)
+    saveToLS();
+    renderFromTodos();
+  }
+
+//очищает список дел
+btnDelAll.addEventListener("click", delAll);
+function delAll() {
+
+    todos = [];
+    saveToLS();
+    renderFromTodos();
+}
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const input = document.querySelector('.input_task');
+// const input = document.querySelector('.input_task');
 const btnAdd = document.querySelector('.add');
 const btnDelCom = document.querySelector('.delete_completed');
 const btnDelAll = document.querySelector('.delete_all');
 const list = document.querySelector('.list');
-const cross = document.querySelector(".cross");
+const textCross = document.querySelector(".cross");
 const checkbox = document.querySelector(".check");
 const text = document.querySelector(".input_task");
 const boxDel = document.querySelector('.box_delete');
 
-
-function addTask() {
-    // let text = input.value;
-    const boxTask = document.createElement('div');
+//Функция, которая рисует список задач
+function addTask(event) {
+    event.preventDefault();
+    list.style = "display: grid";
+    const boxTask = document.createElement('li');
     const checkbox = document.createElement('input');
     const textTask = document.createElement('p');
     const textCross = document.createElement('button');
@@ -23,12 +24,6 @@ function addTask() {
     textTask.className = 'list_text';
     textCross.className = 'cross';
     img.src = "./❌.png";
-    checkbox.addEventListener('change', () => {
-    textTask.classList.toggle("box_task_done");
-    });
-    textCross.addEventListener('click', () => {
-        boxTask.style = 'display: none';
-    });
     textTask.textContent = text.value;
     list.prepend(boxTask);
     boxTask.prepend(label);
@@ -36,10 +31,44 @@ function addTask() {
     boxTask.append(textCross);
     textCross.prepend(img);
     boxDel.style = 'display:block';
+
+//отмечает или отменяет выполнение задачи
+checkbox.addEventListener("change", () => {
+textTask.classList.toggle("box_task_done");
+});
     
-}
+//удаляет задачу при нажатии на крестик 
+        textCross.addEventListener("click", () => {
+          boxTask.style = "display: none";
+        });
+ }
 
 btnAdd.addEventListener('click', addTask)
+
+//удаляет завершенные дела
+function delComplited() {
+    let complited = document.querySelectorAll(".box_task_done");
+//     console.log(complited)
+    complited.forEach((el) => el.parentElement.parentElement.remove());
+//   for (let i = 0; i < complited.length; i++) {
+//     document.querySelector(".box_task").remove();
+//   }
+}
+btnDelCom.addEventListener("click", delComplited);
+
+//очищает список дел
+function delAll() {
+    const boxTask = document.querySelectorAll(".box_task"); 
+    for (let i = 0; i < boxTask.length; i++) {
+      boxTask[i].outerHTML = "";
+    }
+    list.style = "display: none";
+}
+
+btnDelAll.addEventListener("click", delAll);
+
+
+
 
 
 
